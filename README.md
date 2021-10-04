01f8583637aec2bafbd23282a476f636dd768373
Mohamed Maaiz <mohammadh3g@gmail.com>
2021-10-04 20:15:27 +0500

    completed all the basic featurs for beta release

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # library
+
+[Project from The Odin Project](https://www.theodinproject.com/paths/full-stack-javascript/courses/javascript/lessons/library)
+<br><br>[Live preview](https://mohamedmaaiz.github.io/library/)
--- script.js
+++ script.js
@@ -1,40 +1,40 @@
-
 const addBookBTN = document.getElementById('add-book')
 const bookForm = document.querySelector('form')
 const blur = document.getElementById('blur')
 const submitBTN = document.getElementById('book-info')
 const libraryDisplay = document.getElementById('library-display')
-const selectBTN2 = document.querySelector('select')
 
 submitBTN.onclick = () => collectBookInfo()
-addBookBTN.onclick = () => displayFrom()
+addBookBTN.onclick = () => blurMode.on()
 blur.onclick = () => blurMode.off();
 
 let myLibrary = [{
-    title: 'book1',
-    authour: 'writer1',
-    pages: '1',
-    status: 'completed',
+    title: 'The Book',
+    authour: 'Writer',
+    pages: '100',
+    status: 'Droped',
 },{
-    title: 'book2',
-    authour: 'writer2',
-    pages: '2',
-    status: 'completed',
+    title: 'Another Book',
+    authour: 'Writer',
+    pages: '0',
+    status: 'On Hold',
 }]
 
 getLocal()
 displayBooks()
 
+function Book(title, authour, pages, status) {
+    this.title = title
+    this.authour = authour
+    this.pages = pages
+    this.status = status
+}
 
-var blurMode = {
-    off: function(){
-        blur.style.display = 'none'
-        bookForm.style.display = 'none'
-    },
-    on: function(){
-        blur.style.display = 'block'
-        bookForm.style.display = 'flex'
-    }   
+function addBookToLibrary(title, authour, pages, status) {
+    blurMode.off()
+    createDisplayCards(title, authour, pages, status)
+    myLibrary.push(new Book(title, authour, pages, status))
+    setLocal()
 }
 
 function displayBooks() {
@@ -55,12 +55,12 @@ function createDisplayCards(title, authour, pages, status) {
     let pagesP = document.createElement('p')
     let statusP = document.createElement('p')
 
-    removeBTN.textContent = 'Remove'
-    editBTN.textContent = 'Edit'
     titleP.textContent = title
     authourP.textContent = 'Authour: ' + authour
     pagesP.textContent = 'Pages: ' + pages
     statusP.textContent = 'Status: ' + status
+    removeBTN.textContent = 'Remove'
+    editBTN.textContent = 'Edit'
 
     removeBTN.onclick = () => removeBook(title)
     editBTN.onclick = () => editCard(title)
@@ -79,23 +79,19 @@ function editCard(target){
     let index = myLibrary.findIndex(x => x.title === target)
     let obj = myLibrary.find(x => x.title === target)
 
-    let title = obj.title
-    let authour = obj.authour
-    let pages = obj.pages
-
     blurMode.on()
 
     document.querySelector('form span').textContent = index
-    document.getElementById('title').value = title
-    document.getElementById('authour').value = authour
-    document.getElementById('page').value = pages
+    document.getElementById('title').value = obj.title
+    document.getElementById('authour').value = obj.authour
+    document.getElementById('page').value = obj.pages
     document.getElementById('book-info').textContent = 'Update'  
 }
-    
+
 function saveUpdate(title, authour, pages, status, index) {
     const updatedCard = { ...myLibrary[index], title: title, authour:authour, pages: pages, status: status};
     myLibrary[index] = updatedCard
-    console.log(myLibrary[index])
+    blurMode.off()
     displayBooks()
     setLocal()
 }
@@ -105,21 +101,6 @@ function removeBook(target) {
     myLibrary.splice(index, 1)
     displayBooks()
     setLocal()
-}
-
-function Book(title, authour, pages, status) {
-    this.title = title
-    this.authour = authour
-    this.pages = pages
-    this.status = status
-}
-
-function addBookToLibrary(title, authour, pages, status) {
-    blurMode.off()
-    createDisplayCards(title, authour, pages, status)
-    myLibrary.push(new Book(title, authour, pages, status))
-    setLocal()
-    // console.table(myLibrary)
 }
 
 function collectBookInfo() {
@@ -132,10 +113,22 @@ function collectBookInfo() {
     if (validationCheck(title, authour, pages) === false) return
     
     if (document.getElementById('book-info').textContent === 'Update') {
-        let index = document.querySelector('span').textContent
+        let index = document.querySelector('form span').textContent
+        if (updateValidTitle(title, index) === false) return
         return saveUpdate(title, authour, pages, status, index)
     }
     return addBookToLibrary(title, authour, pages, status)
+}
+
+function updateValidTitle(newTitle, index) {
+    let titles = myLibrary.map(x => x.title)
+    titles.splice(index, 1)
+    let check = titles.find(x => x === newTitle)
+
+    if (check) {
+        document.querySelector('.valid-text').innerHTML =  '* Same title exits'
+        return false
+    }
 }
 
 function validationCheck(title, authour, pages) {
@@ -150,34 +143,31 @@ function validationCheck(title, authour, pages) {
     if(title === '') validText += '* Title not Defined<br>'
     if(authour === '') validText += '* Authour not Defined<br>'
     if(pages === '') validText += '* page not Defined'
-    if (title === '' || authour === '' || pages === '' || book) {
+    if (validText !== '') {
         document.querySelector('.valid-text').innerHTML = validText
         return false
     }
 }
 
-function displayFrom() {
-    blurMode.on()
-    document.getElementById('book-info').textContent = 'Submit' 
-}
-
-function setLocal() {
-    clearFrom()
-    updateCounter()
-    localStorage.setItem('MyLibrary', JSON.stringify(myLibrary))
-}
-function getLocal() {
-    myLibrary = JSON.parse(localStorage.getItem('MyLibrary'))
-    updateCounter()
+var blurMode = {
+    off: function(){
+        menu()
+        bookForm.style.display = 'none'
+        clearFrom()
+    },
+    on: function(){
+        blur.style.display = 'block'
+        bookForm.style.display = 'flex'
+    }   
 }
 
 function clearFrom() {
-    document.querySelector('span').textContent = ''
+    document.querySelector('form span').textContent = ''
     document.getElementById('title').value = ''
     document.getElementById('authour').value = ''
     document.getElementById('page').value = ''
     document.querySelector('.valid-text').innerHTML = ''
-    blurMode.off()
+    document.getElementById('book-info').textContent = 'Submit' 
 }
 
 function updateCounter() {
@@ -186,4 +176,28 @@ function updateCounter() {
     document.getElementById('book-want').textContent = myLibrary.filter(x => x.status === 'Want To Read').length
     document.getElementById('book-hold').textContent = myLibrary.filter(x => x.status === 'On Hold').length
     document.getElementById('book-drop').textContent = myLibrary.filter(x => x.status === 'Droped').length 
+}
+
+function setLocal() {
+    clearFrom()
+    updateCounter()
+    localStorage.setItem('MyLibrary', JSON.stringify(myLibrary))
+}
+function getLocal() {
+    if(localStorage.getItem('MyLibrary') === null) setLocal()
+    myLibrary = JSON.parse(localStorage.getItem('MyLibrary'))
+    updateCounter()
+}
+
+let sideBar = document.querySelector('.side-bar')
+function menu() {
+    if (sideBar.classList.contains('active')) {
+        sideBar.classList.remove('active')
+        blur.style.display = 'none'
+    } else if (blur.style.display === 'block') {
+        blur.style.display = 'none'
+    } else {
+        sideBar.classList.add('active')
+        blur.style.display = 'block'
+    }
 }
